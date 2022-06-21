320c1fc6175e71ffda7d7ee6dd41fd0dc8943152
Viktor <lomazavr@gmail.com>
2022-06-21 23:41:58 +0300

    Edit some files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,16 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    es6: true,
   },
   parserOptions: {
-    ecmaVersion: 8,
+    ecmaVersion: 2018,
     sourceType: "module",
     requireConfigFile: false,
   },
   rules: {
     semi: 2,
     "no-debugger": "warn",
+    "no-console": "warn",
   },
 };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,12 @@
 import "../styles/index.scss";
+import svgLogo from "../images/logo.svg";
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
   const root = document.getElementById("root");
-  console.log(root);
+  const logo = document.createElement("img");
+  logo.src = svgLogo;
+  logo.alt = "logo";
+  root.appendChild(logo);
 }
-
